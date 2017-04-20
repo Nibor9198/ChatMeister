032cdd2b175aa0c9ba02669c66e2a149431aaf61
Nibor9198 <robin.boregrim@gmail.com>
2017-04-20 22:49:53 +0200

    Some stuff

--- php/main.php
+++ php/main.php
@@ -1,18 +1,32 @@
 <?php
+session_start();
+
+if(!(isset($_SESSION['UNAME']) && isset($_SESSION['DNAME']) && isset($_SESSION['ID']))){
+    header('Location:../index.php');
+    echo 'hej';
+}
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <link href="css/style.css">
-        <link href="css/.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/main.css">
     </head>
     <body>
+        <nav>
+            <!--<table><tr><td><?php //echo $_SESSION['DNAME']; ?></td></tr></table>-->
+            <ul><li><?php echo $_SESSION['DNAME']; ?></li><li>Thing</li></ul>
+        </nav>
+        
+        
         <?php
+            
+            //echo $_SESSION['ID'];
             
         ?>
     
-    <script src="js/script.js"></script>
+    <script src="../js/script.js"></script>
     </body>
 </html>
--- php/register.php
+++ php/register.php
@@ -19,7 +19,7 @@ if(isset($_POST['uname']) && isset($_POST['psw'])) {
                     if($stmt = $mysqli->prepare($sql)){
                 $stmt->bind_param("sss",$uname, hasha($p), $dname);
                 $stmt->execute();
-                $stmt->bind_result($ID, $Pass);
+                $stmt->bind_result();
                 if($stmt->fetch()){
                     
                 }else{
