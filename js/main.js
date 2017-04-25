9dd731c22d67afd550aa29ae5370ab15cf416ffa
Nibor9198 <robin.boregrim@gmail.com>
2017-04-25 14:59:34 +0200

    Message fetch almost implimented
    Getting displayname almost works

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 function refresh(){
-    alert("hello");
     loadDoc("../php/chat.php", chatResponse, true, 'id=1');
 }
 
 function chatResponse(xhttp){
     alert(xhttp.responseText);
-    
 }
--- php/chat.php
+++ php/chat.php
@@ -3,11 +3,29 @@
 
     if(isset($_POST['id'])){
         if($mysqli = connect_db()){
-            $sql = "select UID, text from chat where Chatid = ?;";
-            $stmt->bind_param("i",$_POST['id']);
-            $stmt->execute();
-            $stmt->bind_result($UID, $text);
-            echo $text[0];
+            $sql = "select UID, text from message where Chatid = ?";
+            if($stmt = $mysqli->prepare($sql)){
+                $stmt->bind_param("i",$_POST['id']);
+                $stmt->execute();
+                $stmt->bind_result($UID, $text);
+                
+                $texts = [];
+                while($stmt->fetch()){
+                    $sql2 = "select DisplayName from user where ID = ?";
+                    $name = "fail";
+                    if($stmt2 = $mysqli->prepare($sql2)){
+                        $stmt2->bind_param("i", $UID);
+                        $stmt2->execute();
+                        $stmt2->bind_result($name);
+                        $stmt2->fetch();
+                        
+                        
+                    }
+                    $texts[] = $name . ": " . $text;
+                }
+                $json = json_encode($texts);
+                echo $json;
+            }
         }    
     }else{
         echo "bajs";
